db7889e17e66275fa9b6d6f86c29f26230836735
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-04-01 16:28:28 +0530

    NewGoal component - added addCourse handler

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import NewGoal from './components/NewGoal/NewGoal.component';
 
 const App = () => {
 
-  const courseGoals = [
+  const myGoals = [
     { id: "cg1", text: "Finish the Course" },
     { id: "cg2", text: "Learn about the Topics" },
     { id: "cg3", text: "Refresh React" },
-  ]
+  ];
+
+  const addCourseHandler = (course) => {
+    myGoals.push(course);
+  }
 
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <GoalList goals={courseGoals} />
-      <NewGoal />
+      <NewGoal addCourse={addCourseHandler} />
+      <GoalList goals={myGoals} />
     </div>
   )
 }
--- src/components/NewGoal/NewGoal.component.jsx
+++ src/components/NewGoal/NewGoal.component.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import './NewGoal.styles.scss';
 
-const NewGoal = () => {
+const NewGoal = ({ addCourse }) => {
+    const [newgoal, setNewgoal] = useState({});
+
+    const inputHandler = (event) => {
+        setNewgoal(
+            {
+                id: Math.random().toString(),
+                text: (event.target.value).toString()
+            })
+    }
+
     return (
-        <div>
-            New Goal component
-        </div>
+        <form className='new-goal' onSubmit={addCourse(newgoal)}>
+            <input
+                type='text'
+                placeholder='Type Here'
+                onChange={inputHandler}
+            />
+            <button type='submit'>Add Goal</button>
+        </form>
     )
 }
 export default NewGoal;
