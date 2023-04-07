6eabcf53b942e921d5102c82510b7cd4d6dfeb93
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-04-07 15:24:05 +0530

    Updated the AppJS with addGoal handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import GoalList from './components/GoalList/GoalList.component';
 import NewGoal from './components/NewGoal/NewGoal.component';
 
 const App = () => {
 
-  const myGoals = [
+  const [courseGoals, setCourseGoals] = useState([
     { id: "cg1", text: "Finish the Course" },
     { id: "cg2", text: "Learn about the Topics" },
     { id: "cg3", text: "Refresh React" },
-  ];
+  ]);
 
-  const addCourseHandler = (course) => {
-    myGoals.push(course);
+  const addGoal = (myGoals) => {
+    setCourseGoals(myGoals);
+    console.log(courseGoals);
   }
 
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <NewGoal addCourse={addCourseHandler} />
-      <GoalList goals={myGoals} />
+      <NewGoal goals={courseGoals} addGoalToList={addGoal} />
+      <GoalList goals={courseGoals} />
     </div>
   )
 }
--- src/components/NewGoal/NewGoal.component.jsx
+++ src/components/NewGoal/NewGoal.component.jsx
@@ -1,23 +1,38 @@
 import { useState } from 'react';
 import './NewGoal.styles.scss';
 
-const NewGoal = ({ addCourse }) => {
-    const [newgoal, setNewgoal] = useState({});
+const NewGoal = ({ goals, addGoalToList }) => {
+    const [count, setCount] = useState(4);
+    const [goal, setGoal] = useState({ id: 'cg' + count, text: "Default Goal" });
 
-    const inputHandler = (event) => {
-        setNewgoal(
-            {
-                id: Math.random().toString(),
-                text: (event.target.value).toString()
-            })
+    const changeHandler = (event) => {
+        setGoal({ id: 'cg' + count, text: event.target.value });
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        let flag = 0;
+        goals.map(i => {
+            if (i.text === event.target.goalName.value) {
+                alert("This goal is already added !!!");
+                flag = 1;
+            }
+        })
+        if (flag === 0) {
+            setCount(count + 1);
+            goals.push(goal);
+            addGoalToList(goals);
+        }
     }
 
     return (
-        <form className='new-goal' onSubmit={addCourse(newgoal)}>
+        <form className='new-goal' onSubmit={submitHandler}>
             <input
                 type='text'
+                id='goalName'
+                name='goalName'
                 placeholder='Type Here'
-                onChange={inputHandler}
+                onChange={changeHandler}
             />
             <button type='submit'>Add Goal</button>
         </form>
